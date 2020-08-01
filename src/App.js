902d8d8c03c73c3609426aac3c3d3938fc05c3b1
Laukesh <laukesh.yadav@dailyhunt.in>
2020-08-01 17:48:29 +0530

    mobxcomplete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+ 
+import {observer,inject} from "mobx-react"
 
+@inject("store")
+@observer
 class App extends Component{
 
   render(){
+    console.log("this is store",this.props.store)
     return(
-      <h1>this is app State</h1>
+      <div>
+        <div>
+          <button onClick={()=>{
+            this.props.store.addCounter()
+            }}>addCounter</button>
+          <button onClick={()=>{
+            this.props.store.totalDeleteCount()
+          }}>deleteCounter</button>
+        </div>
+        <div>
+          {
+            this.props.store.totalCounter.map((element,index)=>{
+              console.log("\n\nelement",element)
+              return (
+                <div key ={index}>
+                  <h1>{element}</h1>
+                  <button onClick={(event)=>{
+                    this.props.store.increament(event,index)
+                    }}>increament</button>
+                    <button onClick ={(event)=>{
+                      this.props.store.decreament(event,index)
+                    }}>decreament</button>
+                    <button onClick ={(event)=>{
+                      this.props.store.deleteCounter(event,index)
+                    }}>deletecounter</button>
+                  </div>
+              )
+            })
+          }
+      </div>
+      </div>
+     
     )
   }
 }
--- src/store/appState.js
+++ src/store/appState.js
@@ -1,8 +1,29 @@
 
-import {observable} from "mobx"
+import {observable,action} from "mobx"
 
 const appState = observable({
-    count : 0
+        count:0,
+    totalCounter : [],
+
+    addCounter:action("counterIncreament",()=>{
+        appState.totalCounter.push(appState.count)
+    }),
+    increament:action("increament",(event,index)=>{
+        //console.log("\n\n appState.counter",appState.totalCounter)
+        //console.log("this is increament index",index)
+        
+       appState.totalCounter[index] +=1
+    }),
+    decreament:action("decreament",(event,index)=>{
+        //console.log("this is decreamented index",index)
+        appState.totalCounter[index] -=1
+    }),
+    deleteCounter:action("deleted Counter",(event,index)=>{
+        appState.totalCounter.splice(index,1)
+    }),
+    totalDeleteCount:action("totalCounterDelete",(event,index)=>{
+        appState.totalCounter =[]
+    })
 })
 
 export default appState
