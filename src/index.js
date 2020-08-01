4e26c5d7a6d3ed08b4d783e36af689225d6b53e4
Laukesh <laukesh.yadav@dailyhunt.in>
2020-08-01 11:46:04 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import appState from './store/appState'
+import {Provider} from "mobx-react"
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store ={appState}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/appState.js
@@ -0,0 +1,8 @@
+
+import {observable} from "mobx"
+
+const appState = observable({
+    count : 0
+})
+
+export default appState
